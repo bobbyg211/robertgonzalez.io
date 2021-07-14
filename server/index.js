00e1d189a32f519baab2318513b32b07d21308c4
bobbyg211 <robertdg96@gmail.com>
2021-07-14 15:24:12 -0400

    Require path error fix

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
+const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
@@ -12,8 +13,8 @@ app.get("/api", (req, res) => {
 });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
